74f45337588fd3a90144087a9d173bfee8be04ec
Roman Tseduliak <51488835+RomanTseduliak@users.noreply.github.com>
2019-12-30 17:32:40 +0200

    Update task2.js

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,38 +1,58 @@
-let numberOfTrying = 3;
+let start = 0;
+let end = 9;
+let maxNumber = 9;
+let numberOfTrying = 4;
 let count = 3;
-let money = 0;
+let prizeBonus = 2;
+let prize100 = 100;
 let currentSum = 0;
-let startMessage = confirm('Do you want to play a game?');
- 
-for (let i = 0; i < numberOfTrying; i++) {
-  if (startMessage) {
-    // eslint-disable-next-line no-var
-    let infoMessage = Number(
-      prompt(`
-    Choose a roulette pocket number from 0 to 8
-    Attemps left: ${count}
-    Total Prize: ${money}$
-    Possible prize on current attemp: ${currentSum}$
-    `)
-    );
-  } else {
-    alert('You did not become a billionaire, but can.');
-  }
-
-  let randomNumber = Math.floor(Math.random() * 2);
-  console.log(infoMessage, randomNumber);
-  if (infoMessage === randomNumber) {
-    alert('You guess the number.You won prize');
-    if (count === 3) {
-      currentSum = currentSum + 100;
-    } else if (count === 2) {
-      currentSum = currentSum + 50;
+let randomNum = Math.floor(Math.random() * (end - start) + start);
+if (confirm('Do you want to play a game?')) {
+  for (let i = count; i > start; i--) {
+    let userNumber = +prompt(`Choose a roulette pocket number from 0 to 8
+     Attempts left: ${i}
+     Total prize: ${currentSum}$
+     Possible prize on current attempt: ${prize100}$`);
+    if (randomNum === userNumber) {
+      currentSum += prize100;
+      if (
+        confirm(
+          `Congratulation, you won! Your prize is: ${currentSum} $. Do you want to continue?`
+        )
+      ) {
+        end += numberOfTrying;
+        randomNum = Math.floor(Math.random() * (end - start) + start);
+        prize100 *= prizeBonus;
+        i = count;
+      } else {
+        alert(
+          `Thank you for your participation. Your prize is: ${currentSum} $`
+        );
+        if (confirm('Do you want to play again?')) {
+          end = maxNumber;
+          randomNum = Math.floor(Math.random() * (end - start) + start);
+          i = count;
+          currentSum = 0;
+        } else {
+          break;
+        }
+      }
     } else {
-      currentSum = currentSum + 25;
+      if (i !== 1) {
+        prize100 /= prizeBonus;
+      } else {
+        currentSum = 0;
+        alert(
+          `Thank you for your participation. Your prize is: ${currentSum}$`
+        );
+        if (confirm('Do you want to play again?')) {
+          end = maxNumber;
+          randomNum = Math.floor(Math.random() * (end - start) + start);
+          i = count;
+        }
+      }
     }
-  } else {
-    alert('Sorry,you lose');
-    count--;
-    console.log(count);
   }
+} else {
+  alert('You did not become a billionaire, but can');
 }
