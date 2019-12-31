3beced0ff2ee1b1d67dcc9128a7159c4992172d6
Roman Tseduliak <51488835+RomanTseduliak@users.noreply.github.com>
2019-12-31 20:50:55 +0200

    Update countNumbers.js

--- FL12_HW8/homework/js/countNumbers.js
+++ FL12_HW8/homework/js/countNumbers.js
@@ -1,1 +1,12 @@
-//
+function weAreNombers(token) {
+  let count = '';
+  for (let i = 0; i < token.length; i++) {
+    if (!isNaN(parseInt(count[i]))) {
+      count += token[i];
+    }
+  }
+  return count;
+}
+
+weAreNombers();
+
