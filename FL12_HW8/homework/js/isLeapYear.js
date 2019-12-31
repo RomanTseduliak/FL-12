ec710a4879057f902a7bf1aa207d77b3f8dd345f
Roman Tseduliak <51488835+RomanTseduliak@users.noreply.github.com>
2019-12-31 10:11:29 +0200

    Update isLeapYear.js

--- FL12_HW8/homework/js/isLeapYear.js
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -1,9 +1,13 @@
-function isLeapYear(year) {
+function isLeap(year) {
   let isLeapYear = new Date(year, 1, 29).getDate() === 29;
 
   if (isNaN(year)) {
     console.log('Invalid Date');
+  } else if (isLeapYear) {
+    console.log(`${year} is a leap year`);
+  } else {
+    console.log(`${year} is not a leap year`);
   }
-  return isLeapYear ? `${year} is a leap year` : `${year} is not a leap year`;
+  return isLeapYear;
 }
-isLeapYear();
+console.log(isLeap('fdsfs'));
